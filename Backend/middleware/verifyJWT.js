731ca35b9e1f24e452db11085bad472d9c6c0f5b
AdvenAdam <advenadam@student.uns.ac.id>
2023-03-19 22:48:56 +0700

    fix verify JWT

--- Backend/middleware/verifyJWT.js
+++ Backend/middleware/verifyJWT.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization || req.headers.Authorization
 
-    if (authHeader?.startsWith('Bearer ')) {
+    if (!authHeader?.startsWith('Bearer ')) {
         return res.status(401).json({ message: 'Unauthorized' })
     }
 
@@ -13,13 +13,12 @@ const verifyJWT = (req, res, next) => {
         token,
         process.env.ACCESS_TOKEN_SECRET,
         (err, decoded) => {
-            if (err) return res.status(403).json({ message: 'Forbiden' })
-            req.user = decoded.UserInfo.username;
-            req.roles = decoded.UserInfo.roles;
-            next();
+            if (err) return res.status(403).json({ message: 'Forbidden' })
+            req.user = decoded.UserInfo.username
+            req.roles = decoded.UserInfo.roles
+            next()
         }
     )
-
 }
 
 module.exports = verifyJWT
